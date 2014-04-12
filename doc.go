0140b08ed44296520e8f8154efe9a0442508de78
Rishabh Manocha <rmanocha@gmail.com>
2014-04-12 14:25:28 -0700

    Adding a file for documentation

---
+++ doc.go
@@ -0,0 +1,3 @@
+// Package go_newrelic_api is a Go library for Newrelic's API. It allows you to request data from all the GET API calls defined by NewRelic (more at https://docs.newrelic.com/docs/features/getting-started-with-the-new-relic-rest-api).
+
+package go_newrelic_api
